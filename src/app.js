3207638777214dc457a02953732d171ffc6992d8
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-09-04 23:28:45 +0000

    Finale save

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  //console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//write your code here
+//console.log("Hello Rigo from the console!");
+//};
 const domainNames = () => {
   let one = ["the", "our", "their", "this"];
   let two = ["great", "big", "wrathful", "meek"];
@@ -16,13 +16,13 @@ const domainNames = () => {
   let four = [".com", ".org", ".gov", ".net"];
 
   for (let i = 0; i < one.length; i++) {
-    for (let a = 0; a < two.length; i++) {
-      for (let b = 0; b < three.length; i++) {
-        for (let c = 0; c < four.length; i++) {
+    for (let a = 0; a < two.length; a++) {
+      for (let b = 0; b < three.length; b++) {
+        for (let c = 0; c < four.length; c++) {
           console.log(one[i] + two[a] + three[b] + four[c]);
         }
       }
     }
   }
 };
-console.log(domainNames);
+console.log(domainNames());
