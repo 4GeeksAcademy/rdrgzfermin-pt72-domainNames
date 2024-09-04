226d8994414291709d9e3bead5a259a28cf095c2
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-09-04 23:23:03 +0000

    next

--- src/app.js
+++ src/app.js
@@ -7,22 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
 };
-const generateDomainNames = () => {
+const domainNames = () => {
   let one = ["the", "our", "their", "this"];
   let two = ["great", "big", "wrathful", "meek"];
   let three = ["jogger", "racoon", "passion", "estate"];
   let four = [".com", ".org", ".gov", ".net"];
 
-  for (let i = 0; i < one.lenght; i++) {
-    for (let a = 0; a < two.lenght; i++) {
-      for (let b = 0; b < three.lenght; i++) {
-        for (let c = 0; c < four.lenght; i++) {
+  for (let i = 0; i < one.length; i++) {
+    for (let a = 0; a < two.length; i++) {
+      for (let b = 0; b < three.length; i++) {
+        for (let c = 0; c < four.length; i++) {
           console.log(one[i] + two[a] + three[b] + four[c]);
         }
       }
     }
   }
 };
-console.log(generateDomainNames);
+console.log(domainNames);
